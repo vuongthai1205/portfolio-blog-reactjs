e0864661abf5b62d2600552b7e183cb742b471e6
vuongthai1205 <2051010367vuong@ou.edu.vn>
2023-11-02 23:36:26 +0700

    #12 - 90% add and read work

--- src/pages/WorkDetail.js
+++ src/pages/WorkDetail.js
@@ -1,5 +1,34 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { useParams } from "react-router";
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useEffect, useState } from "react";
+
+import parse from 'html-react-parser';
 function WorkDetail() {
-  return ( <>work</> );
+  const workId = useParams("id");
+  const [work, setWork] = useState({
+
+  })
+  useEffect(() => {
+    const db = getDatabase();
+    const starCountRef = ref(db, "works/" + workId.id );
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+      setWork(data)
+    });
+  }, []);
+  return (
+    <Container>
+      <Row>
+        <Col>
+          {work.title}
+          <img src={work.image} alt={work.title} width={300} className="mx-auto"/>
+          {parse(work.content)}
+
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default WorkDetail;
